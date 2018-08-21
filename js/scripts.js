fb0ccfbc4af9e2f5bb3a265dbce3c376b9dff8e8
Jasmine Carbajal <jaz@Jasmines-MacBook-Pro.local>
2018-08-20 20:53:49 -0700

    day 3 updates

--- js/scripts.js
+++ js/scripts.js
@@ -14,39 +14,142 @@ let myAge= 24;
 console.log(`Hello, ${myName}! You are ${myAge} years old.`);
 
 // DAY 2 - DATE
-const today = new Date();
-let day = today.getDate();
-let month = today.getMonth() + 1;
-let year = today.getFullYear();
-let hour = today.getHours();
+// const today = new Date();
+// let day = today.getDate();
+// let month = today.getMonth() + 1;
+// let year = today.getFullYear();
+// let hour = today.getHours();
 // console.log(today);
 //  CONSOLE ES5 WAY
 // console.log(day, month, year, hour);
 
 // CONSOLE THE ES6 way
-console.log(`${month}/${day}/${year}`)
+// console.log(`${month}/${day}/${year}`)
 
 
 // Day 2 -- MATH
+// NOTE!
 // gets random number between 0 and 1
 // const rand = Math.random();
 // Give us a number, then multiply it by ten.
 // Nes add 1 so we cannot get 0. Finally round down the number.
-const rand = Math.floor((Math.random() *10) +1);
+// const rand = Math.floor((Math.random() *10) +1);
 // console.log(rand);
 
 // simple pop up
 // alert(rand);
 
+// NOTE!
 // user clicks to cancel or okay
 // confirm(rand);
 
 // accepts a users input alerts the name.
 // prompt(rand);
-let name = prompt("Please enter your first name.");
-let lastName = prompt("Please enter your last name.");
-console.log(name);
-alert(lastName);
-var date = prompt("What is your Date Of Birth?");
-if (window.confirm("Is this your Date Of Birth? " + date));
+// let name = prompt("Please enter your first name.");
+// let lastName = prompt("Please enter your last name.");
+// console.log(name);
+// alert(lastName);
+// var date = prompt("What is your Date Of Birth?");
+// if (window.confirm("Is this your Date Of Birth? " + date));
 // let name = prompt('Please enter last name.')
+
+// DAY 3 EXCERSIZES
+// NOTE!!
+//  === strict comparison
+//  == loose comparison
+//  ! not TRUE
+// !== stict not comparison
+//  != loose not comparison
+//  > greater than
+// < Less than
+// >= greater than or equal
+// <= less than or equal to
+
+// const myNumber = '1';
+// const userInput = prompt("Please enter a numbre: ");
+// if (1 == userInput) {
+  // console.log("I am here");
+  // console.log(!true); (NOT TRUE)
+
+// } else {
+  // console.log ("In the else")
+// }
+
+//  DAY 3 LOGICAL OPERATORS
+// const userInput= prompt('Please enter a number:');
+// if (userInput == 1 || userInput == 5 || userInput == 15) {
+//   console.log('I am here');
+// } else{
+//   console.log ('In the else')
+// }
+
+// if (userInput > 5 && userInput < 10){
+//   console.log ('I am here');
+// } else {
+//   console.log ('In the else.');
+// }
+
+//  DAY 3 -- ELSE IF
+// if (userInput > 5) {
+//   console.log ('Number is greater than five');
+// } else if (userInput < 5){
+//   console.log ('Number is less than five');
+// } else if (userInput === '5'){
+//   console.log ('User entered a five')
+// } else if (userInput == '' || userInput == null){
+//   console.log ('User input nothing');
+// } else {
+//   console.log ('User did not enter a number');
+// }
+
+//  DAY 3 -- switch
+
+// const today = new Date().getDay();
+// console.log(today);
+// switch (today) {
+//   case 0:
+//     console.log('Sunday');
+//     break;
+//   case 1:
+//     console.log('Monday');
+//     break;
+//   case 2:
+//     console.log('Tuesday');
+//     break;
+//   case 3:
+//     console.log('Wednesday');
+//     break;
+//   case 4:
+//     console.log('Thursday');
+//     break;
+//   case 5:
+//     console.log('Friday');
+//     break;
+//   case 6:
+//     console.log('Satursday');
+//     break;
+// }
+
+//  DAY 3 -- MATH
+// const myNumber = 1 + 1;
+// const subtraction = 2 - 1;
+// const multiplication = 2 * 2;
+// const division = 10 / 2;
+// const remainder = 11 % 3;
+// console.log(addition);
+
+
+let myNum = 1;
+// INCREMENT (NOTE)
+// myNum++;
+// DECREMENT (NOTE)
+// myNum--;
+// myNum = myNumb + 3; (WRONG) (reassigning variable)
+// (CORRECT) (maniplulating variable)
+myNum +=3;
+myNum -=10;
+myNum *=5;
+myNum /=12;
+
+
+console.log(myNum);
